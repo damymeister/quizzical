9ee16637096e1920238543e770534c9c6b45ca8f
Mateusz <mattiadamczyk10@gmail.com>
2023-04-27 17:37:20 +0200

    Updates

---
+++ precache-manifest.c24c8042d820e95d0f2c6baa666855c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "212fe50c378f7d5fd503",
+    "url": "/quizzical/static/css/main.ebd9ffeb.chunk.css"
+  },
+  {
+    "revision": "212fe50c378f7d5fd503",
+    "url": "/quizzical/static/js/main.212fe50c.chunk.js"
+  },
+  {
+    "revision": "af059338938fe06784a0",
+    "url": "/quizzical/static/js/1.af059338.chunk.js"
+  },
+  {
+    "revision": "10828be3562dbc652641",
+    "url": "/quizzical/static/js/runtime~main.10828be3.js"
+  },
+  {
+    "revision": "be981d96d856820d4688747b4f4db0f1",
+    "url": "/quizzical/index.html"
+  }
+];

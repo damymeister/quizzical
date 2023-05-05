323c4255ea35fa7a80ddf950ca02064bb26bf067
Mateusz <mattiadamczyk10@gmail.com>
2023-05-05 14:15:38 +0200

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Home from "./components/Home"
 import Quiz from "./components/Quiz"
-
+import Navbar from "./components/Navbar"
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 export default function App() {
   const [allData, setAllData] = React.useState([])
   const [quizStart, setQuizStart] = React.useState(false)
@@ -19,7 +20,8 @@ export default function App() {
   return (
     <div className="app-main">
       <div className="container">
-        {quizStart && allData.length !==0 ? <Quiz allData={allData} Quizreset = {Quizreset} /> : <Home viewChange={viewChange} />}
+        <Navbar />
+      {quizStart && allData.length !==0 ? <Quiz allData={allData} Quizreset = {Quizreset} /> : <Home viewChange={viewChange} />}
       </div>
     </div>
   )
---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Navbar(){
+    return(
+        <div className="navbar-main">
+            <button className="login">Log in</button>
+            <button className="register">Register</button>
+        </div>
+    )
+}
